514f3db7d1e189643596881a2b2d0b1a29ec6313
marvin <marvin>
2023-07-20 10:16:04 +0800

    feat: cleanup code

--- test/helpers/tx.ts
+++ test/helpers/tx.ts
@@ -1,11 +1,11 @@
-import {ethers} from "hardhat";
+import { ethers } from 'hardhat';
 
 export const waitForTx = async (tx) => {
   await tx.wait(1);
-}
+};
 
 export const latestBlockNumber = async (): Promise<number> => {
-  const block = await ethers.provider.getBlock("latest");
+  const block = await ethers.provider.getBlock('latest');
   if (!block) {
     throw `latestBlockNumber: missing block`;
   }
@@ -14,7 +14,7 @@ export const latestBlockNumber = async (): Promise<number> => {
 
 export const getBlockTimestamp = async (blockNumber?: number): Promise<number> => {
   if (!blockNumber) {
-    const block = await ethers.provider.getBlock("latest");
+    const block = await ethers.provider.getBlock('latest');
     if (!block) {
       throw `getBlockTimestamp: missing block number ${blockNumber}`;
     }
