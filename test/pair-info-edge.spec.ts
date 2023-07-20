d2126ec46698bb745c3d10fb1ed62527127046a8
marvin <marvin@crus.xyz>
2023-07-20 10:16:04 +0800

    feat: cleanup code

--- test/pair-info-edge.spec.ts
+++ test/pair-info-edge.spec.ts
@@ -1,32 +1,25 @@
-import {testEnv} from "./helpers/make-suite";
-import {ethers} from "hardhat";
-import {describe} from "mocha";
+import { testEnv } from './helpers/make-suite';
+import { ethers } from 'hardhat';
+import { describe } from 'mocha';
 
 describe('PairInfo: Edge cases', () => {
+  before('Deploy Pair', async () => {
+    const { deployer } = testEnv;
+    console.log(`deployer address:`, deployer.address);
 
-    before('Deploy Pair', async () => {
-        const {deployer} = testEnv;
-        console.log(`deployer address:`, deployer.address);
+    const pairInfoFactory = await ethers.getContractFactory('PairInfo', deployer.signer);
+    const pairInfo = await pairInfoFactory.deploy();
 
-        const pairInfoFactory = await ethers.getContractFactory('PairInfo', deployer.signer);
-        const pairInfo = await pairInfoFactory.deploy();
+    console.log(`pairInfo address:`, pairInfo.address);
+  });
 
-        console.log(`pairInfo address:`, pairInfo.address);
-    });
+  it('check getters', async () => {});
 
-    it('check getters', async () => {
+  describe('test addPair', async () => {
+    it('check pair info ', async () => {});
+  });
 
-    });
-
-    describe('test addPair', async () => {
-        it('check pair info ', async () => {
-
-        });
-    });
-
-    describe('test updatePair', async () => {
-        it('check configs', async () => {
-
-        });
-    });
+  describe('test updatePair', async () => {
+    it('check configs', async () => {});
+  });
 });
