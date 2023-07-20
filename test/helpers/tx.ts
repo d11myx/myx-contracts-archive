145353d08a8d4a0b16d3a9c5a56fa4ffd924805a
marvin <marvin@crus.xyz>
2023-07-20 14:45:25 +0800

    feat: pair-info-edge.test

--- test/helpers/tx.ts
+++ test/helpers/tx.ts
@@ -1,7 +1,40 @@
 import { ethers } from 'hardhat';
+import { BaseContract, Contract, ContractTransaction } from 'ethers';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
 
-export const waitForTx = async (tx) => {
-  await tx.wait(1);
+declare var hre: HardhatRuntimeEnvironment;
+
+export const waitForTx = async (tx: ContractTransaction) => await tx.wait(1);
+
+export const deployContract = async <ContractType extends Contract>(
+  contract: string,
+  args?: any,
+): Promise<ContractType> => {
+  const [deployer] = await ethers.getSigners();
+
+  const contractFactory = await hre.ethers.getContractFactory(contract, deployer);
+  const contractDeployed = await contractFactory.deploy(...args);
+
+  return (await hre.ethers.getContractAt(contract, contractDeployed.address)) as any as ContractType;
+};
+
+export const deployUpgradeableContract = async <ContractType extends Contract>(
+  contract: string,
+  args?: any,
+): Promise<ContractType> => {
+  const [deployer] = await ethers.getSigners();
+
+  const contractFactory = await hre.ethers.getContractFactory(contract, deployer);
+  let contractDeployed = await hre.upgrades.deployProxy(contractFactory, [...args]);
+
+  return (await hre.ethers.getContractAt(contract, contractDeployed.address)) as any as ContractType;
+};
+
+export const getContractAt = async <ContractType extends BaseContract>(
+  contract: string,
+  address: string,
+): Promise<ContractType> => {
+  return (await hre.ethers.getContractAt(contract, address)) as any as ContractType;
 };
 
 export const latestBlockNumber = async (): Promise<number> => {
--- test/pair-info-edge.spec.ts
+++ test/pair-info-edge.spec.ts
@@ -1,25 +1,65 @@
 import { testEnv } from './helpers/make-suite';
-import { ethers } from 'hardhat';
 import { describe } from 'mocha';
+import { expect } from './shared/expect';
+import { pairConfigs } from './shared/config';
+import { waitForTx } from './helpers/tx';
 
 describe('PairInfo: Edge cases', () => {
-  before('Deploy Pair', async () => {
-    const { deployer } = testEnv;
-    console.log(`deployer address:`, deployer.address);
+  // before('deploy Pair', async () => {
+  //   const { deployer } = testEnv;
+  //   console.log(`deployer address:`, deployer.address);
+  //
+  //   const pairInfoFactory = await ethers.getContractFactory('PairInfo', deployer.signer);
+  //   const pairInfo = await pairInfoFactory.deploy();
+  //
+  //   console.log(`pairInfo address:`, pairInfo.address);
+  // });
 
-    const pairInfoFactory = await ethers.getContractFactory('PairInfo', deployer.signer);
-    const pairInfo = await pairInfoFactory.deploy();
+  it('check getters', async () => {
+    const { deployer, pairInfo, pairLiquidity } = testEnv;
 
-    console.log(`pairInfo address:`, pairInfo.address);
+    expect(await pairInfo.pairLiquidity()).to.be.eq(pairLiquidity.address);
   });
 
-  it('check getters', async () => {});
-
   describe('test addPair', async () => {
-    it('check pair info ', async () => {});
+    before('addPair', async () => {
+      const { deployer, pairInfo, btc, usdt } = testEnv;
+
+      const btcPair = pairConfigs['BTC_USDT'];
+
+      const pair = btcPair.pair;
+      pair.indexToken = btc.address;
+      pair.stableToken = usdt.address;
+      const tradingConfig = btcPair.tradingConfig;
+      const tradingFeeConfig = btcPair.tradingFeeConfig;
+      const fundingFeeConfig = btcPair.fundingFeeConfig;
+
+      await waitForTx(await pairInfo.addPair(pair, tradingConfig, tradingFeeConfig, fundingFeeConfig));
+
+      expect(await pairInfo.pairsCount()).to.be.eq(1);
+    });
+
+    it('check pair info', async () => {
+      const { pairInfo, btc, usdt } = testEnv;
+
+      const pairIndex = 0;
+      // console.log(await pairInfo.pairs(pairIndex));
+      // console.log(await pairInfo.getPair(pairIndex));
+
+      expect(await pairInfo.pairs(pairIndex)).deep.be.eq(await pairInfo.getPair(pairIndex));
+      expect(await pairInfo.tradingConfigs(pairIndex)).deep.be.eq(await pairInfo.getTradingConfig(pairIndex));
+      expect(await pairInfo.tradingFeeConfigs(pairIndex)).deep.be.eq(await pairInfo.getTradingFeeConfig(pairIndex));
+      expect(await pairInfo.fundingFeeConfigs(pairIndex)).deep.be.eq(await pairInfo.getFundingFeeConfig(pairIndex));
+
+      const btcPair = pairConfigs['BTC_USDT'];
+      const pair = await pairInfo.getPair(pairIndex);
+      expect(pair.indexToken).to.be.eq(btc.address);
+      expect(pair.stableToken).to.be.eq(usdt.address);
+      expect(pair.enable).to.be.eq(btcPair.pair.enable);
+    });
   });
 
   describe('test updatePair', async () => {
-    it('check configs', async () => {});
+    it('check updated pair', async () => {});
   });
 });
