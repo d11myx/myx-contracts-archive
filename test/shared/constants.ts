4849b4c0a1afb7d851c137770fe755ca4a5755d1
marvin <marvin>
2023-07-25 11:24:20 +0800

    feat: setup deployPrice

--- test/shared/constants.ts
+++ test/shared/constants.ts
@@ -1,5 +1,6 @@
 import { ethers } from 'hardhat';
 import { parseUnits } from 'ethers/lib/utils';
+import { BigNumber } from 'ethers';
 
 export const ONE_ETHER = ethers.utils.parseEther('1');
 export const MAX_UINT_AMOUNT = ethers.constants.MaxUint256;
@@ -17,7 +18,13 @@ export enum eBscNetwork {
 
 export type eNetwork = eEthereumNetwork | eBscNetwork;
 
-export const MOCK_PRICES: { [key: string]: string } = {
-  BTC: parseUnits('30000', 8).toString(),
-  ETH: parseUnits('2000', 8).toString(),
+export const Market = 'USDT';
+
+export function getMarketSymbol(): string {
+  return Market;
+}
+
+export const MOCK_PRICES: { [key: string]: BigNumber } = {
+  BTC: parseUnits('30000', 8),
+  ETH: parseUnits('2000', 8),
 };
