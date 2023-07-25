8f1302e1f49063fb23b4b88acb2f12d5f1fc2f8f
marvin <marvin>
2023-07-25 13:19:20 +0800

    feat: setup deployTrading

--- test/helpers/contract-deployments.ts
+++ test/helpers/contract-deployments.ts
@@ -1,5 +1,5 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { Token, VaultPriceFeedTest, WETH } from '../../types/ethers-contracts';
+import { Token, VaultPriceFeedTest, WETH } from '../../types';
 import { deployContract } from './tx';
 
 declare var hre: HardhatRuntimeEnvironment;
@@ -10,8 +10,4 @@ export const deployMockToken = async (symbol: string): Promise<Token> => {
 
 export const deployWETH = async (): Promise<WETH> => {
   return await deployContract<WETH>('WETH', ['WETH', 'WETH', '18']);
-};
-
-export const deployVaultPriceFeed = async (): Promise<VaultPriceFeedTest> => {
-  return await deployContract<VaultPriceFeedTest>('VaultPriceFeedTest', ['WETH', 'WETH', '18']);
 };
