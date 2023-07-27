9e2abf11b44240488008e68e534aaf525cb685b7
defiiron <defiiron@gmail.com>
2023-07-27 14:21:37 +0800

    Use @openzeppelin

--- contracts/libraries/PrecisionUtils.sol
+++ contracts/libraries/PrecisionUtils.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "../openzeeplin/contracts/utils/math/Math.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
 
 library PrecisionUtils {
 
