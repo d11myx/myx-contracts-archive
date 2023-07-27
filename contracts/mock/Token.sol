fa1b3d170ef5c1b6292eeaab2b9c5a116c9d0725
defiiron <defiiron@gmail.com>
2023-07-27 14:53:00 +0800

    Use IERC20

--- contracts/mock/Token.sol
+++ contracts/mock/Token.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "./interfaces/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "hardhat/console.sol";
 
