64ff83ee548dc332f0da426d242358db98f3bae8
Fredy <fredy_xiang@163.com>
2023-07-29 00:28:01 +0800

    sync block time

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -82,9 +82,16 @@ task('encode-event', 'get method artifact detail by method name')
 task('update-evm-time', 'update evm time')
     .addParam('increase', 'increase or decrease minutes')
     .setAction(async (param, hre) => {
+        let blockNumber = await hre.ethers.provider.getBlockNumber()
+        let block = await hre.ethers.provider.getBlock(blockNumber)
+        console.log(`block time ${block.timestamp} diff ${block.timestamp - getCurrentTimestamp()}`)
+
         await hre.network.provider.send('evm_increaseTime', [parseInt(param.increase)]);
-        const blockNumber = await hre.ethers.provider.getBlockNumber()
-        const block = await hre.ethers.provider.getBlock(blockNumber)
+
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+
+        blockNumber = await hre.ethers.provider.getBlockNumber()
+        block = await hre.ethers.provider.getBlock(blockNumber)
         console.log(`block time ${block.timestamp} diff ${block.timestamp - getCurrentTimestamp()}`)
     });
 
