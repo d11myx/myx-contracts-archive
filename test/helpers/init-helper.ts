be4f0baaf65eafd6eec1eb950a766f7acc4209fc
defiiron <defiiron@gmail.com>
2023-07-28 10:00:43 +0800

    Format ts code

--- test/helpers/init-helper.ts
+++ test/helpers/init-helper.ts
@@ -6,30 +6,30 @@ import { getMarketSymbol } from '../shared/constants';
 import { SymbolMap } from '../shared/types';
 
 export async function initPairs(
-  deployer: SignerWithAddress,
-  pairTokens: SymbolMap<Token>,
-  usdt: Token,
-  pairInfo: PairInfo,
+    deployer: SignerWithAddress,
+    pairTokens: SymbolMap<Token>,
+    usdt: Token,
+    pairInfo: PairInfo,
 ) {
-  console.log(`Initializing pairs`);
-  const pairConfigs = loadPairConfigs(getMarketSymbol());
+    console.log(`Initializing pairs`);
+    const pairConfigs = loadPairConfigs(getMarketSymbol());
 
-  for (let symbol of Object.keys(pairConfigs)) {
-    const pairConfig = pairConfigs[symbol];
-    const pair = pairConfig.pair;
-    pair.indexToken = pairTokens[symbol].address;
-    pair.stableToken = usdt.address;
-    const tradingConfig = pairConfig.tradingConfig;
-    const tradingFeeConfig = pairConfig.tradingFeeConfig;
-    const fundingFeeConfig = pairConfig.fundingFeeConfig;
+    for (let symbol of Object.keys(pairConfigs)) {
+        const pairConfig = pairConfigs[symbol];
+        const pair = pairConfig.pair;
+        pair.indexToken = pairTokens[symbol].address;
+        pair.stableToken = usdt.address;
+        const tradingConfig = pairConfig.tradingConfig;
+        const tradingFeeConfig = pairConfig.tradingFeeConfig;
+        const fundingFeeConfig = pairConfig.fundingFeeConfig;
 
-    await waitForTx(await pairInfo.addPair(pair, tradingConfig, tradingFeeConfig, fundingFeeConfig));
+        await waitForTx(await pairInfo.addPair(pair, tradingConfig, tradingFeeConfig, fundingFeeConfig));
 
-    console.log(
-      `added pair [${symbol}, ${getMarketSymbol()}] at index`,
-      (await pairInfo.pairsCount()).sub(1).toString(),
-    );
-  }
+        console.log(
+            `added pair [${symbol}, ${getMarketSymbol()}] at index`,
+            (await pairInfo.pairsCount()).sub(1).toString(),
+        );
+    }
 
-  console.log(`Configured all pairs [${Object.keys(pairConfigs)}]`);
+    console.log(`Configured all pairs [${Object.keys(pairConfigs)}]`);
 }
