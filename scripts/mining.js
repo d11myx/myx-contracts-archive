ed8f29cae5e7b2fe7fc3af3f30fa6d8a36e6069e
Fredy <fredy_xiang@163.com>
2023-07-29 00:28:01 +0800

    sync block time

--- scripts/mining.js
+++ scripts/mining.js
@@ -1,6 +1,23 @@
 const hre = require('hardhat');
+const {getBlockTime} = require("./utils/utilities");
+const {getCurrentTimestamp} = require("hardhat/internal/hardhat-network/provider/utils/getCurrentTimestamp");
+
 async function main() {
     await hre.network.provider.send('evm_setIntervalMining', [3000]);
+
+    // sync block time
+    let blockTime = await getBlockTime(await hre.ethers.provider);
+    let currentTime = getCurrentTimestamp();
+    let diff = currentTime - blockTime;
+    console.log(`current time ${currentTime} block time ${blockTime} diff ${diff}`)
+    await hre.network.provider.send('evm_increaseTime', [diff]);
+
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    blockTime = await getBlockTime(await hre.ethers.provider);
+    currentTime = getCurrentTimestamp();
+    diff = currentTime - blockTime;
+    console.log(`current time ${currentTime} block time ${blockTime} diff ${diff}`)
 }
 
 main()
