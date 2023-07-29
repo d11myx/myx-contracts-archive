9534bfafedc08633a4cfc65ebc8c93415761499f
defiiron <defiiron@gmail.com>
2023-07-29 17:43:42 +0800

    Remove unuse code

--- contracts/price/interfaces/IFastPriceFeed.sol
+++ contracts/price/interfaces/IFastPriceFeed.sol
@@ -10,7 +10,6 @@ interface IFastPriceFeed {
 
     function setMaxPriceUpdateDelay(uint256 _maxPriceUpdateDelay) external;
     
-    function setMinBlockInterval(uint256 _minBlockInterval) external;
 
     function setPricesWithBits(uint256 _priceBits, uint256 _timestamp) external;
 }
