a9d493ac9421301d794ae39fdc6896f708705ebe
marvin <marvin@crus.xyz>
2023-08-08 11:37:02 +0800

    merged

--- contracts/libraries/Int256Utils.sol
+++ contracts/libraries/Int256Utils.sol
@@ -8,9 +8,18 @@ library Int256Utils {
 
     using Strings for uint256;
 
-    function abs(int256 amount) internal pure returns(uint256) {
-        return amount >= 0 ? uint256(amount) : uint256(- amount);
+    function abs(int256 a) internal pure returns(uint256) {
+        return a >= 0 ? uint256(a) : uint256(- a);
     }
+
+    function min(int256 a, int256 b) internal pure returns(int256) {
+        return a < b ? a : b;
+    }
+
+    function max(int256 a, int256 b) internal pure returns(int256) {
+        return a > b ? a : b;
+    }
+
 
     function toString(int256 amount) internal pure returns(string memory) {
         return string.concat(amount >= 0 ? "" : "-", abs(amount).toString());
