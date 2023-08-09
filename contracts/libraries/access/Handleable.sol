dc831ae513fc8f6ab3b0b2f59ed290697189f219
defiiron <defiiron@gmail.com>
2023-08-09 11:34:36 +0800

    Remove setHandler

--- contracts/libraries/access/Handleable.sol
+++ contracts/libraries/access/Handleable.sol
@@ -1,37 +1,26 @@
 // SPDX-License-Identifier: MIT
-import "./Governable.sol";
-import "../../interfaces/IAddressesProvider.sol";
-import "../../interfaces/IRoleManager.sol";
-
 pragma solidity ^0.8.0;
 
-abstract contract Handleable {
+import '../../interfaces/IAddressesProvider.sol';
+import '../../interfaces/IRoleManager.sol';
 
+abstract contract Handleable {
     mapping(address => bool) public isHandler;
 
     IAddressesProvider public immutable ADDRESS_PROVIDER;
 
-    modifier onlyHandler() {
-        require(msg.sender == address(this) || isHandler[msg.sender], "Handleable: forbidden");
-        _;
-    }
-
     modifier onlyAdmin() {
-        require(IRoleManager(ADDRESS_PROVIDER.getRoleManager()).isAdmin(msg.sender), "onlyAdmin");
+        require(IRoleManager(ADDRESS_PROVIDER.getRoleManager()).isAdmin(msg.sender), 'onlyAdmin');
         _;
     }
 
     modifier onlyPoolAdmin() {
-        require(IRoleManager(ADDRESS_PROVIDER.getRoleManager()).isPoolAdmin(msg.sender), "onlyPoolAdmin");
+        require(IRoleManager(ADDRESS_PROVIDER.getRoleManager()).isPoolAdmin(msg.sender), 'onlyPoolAdmin');
         _;
     }
 
     constructor(IAddressesProvider addressProvider) {
         isHandler[msg.sender] = true;
         ADDRESS_PROVIDER = addressProvider;
-    }
-
-    function setHandler(address _handler, bool _isHandler) public onlyAdmin {
-        isHandler[_handler] = _isHandler;
     }
 }
