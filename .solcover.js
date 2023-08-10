b96d7d797af8e39ce04b99a557b0bc2c9163f294
defiiron <defiiron@gmail.com>
2023-08-10 19:32:52 +0800

    Use solcOptimizerDetails

--- .solcover.js
+++ .solcover.js
@@ -2,4 +2,14 @@ module.exports = {
     allowUnlimitedContractSize: true,
     configureYulOptimizer: true,
     skipFiles: ['mock', 'openzeeplin'],
+    solcOptimizerDetails: {
+        peephole: false,
+        inliner: false,
+        jumpdestRemover: false,
+        orderLiterals: true,
+        deduplicate: false,
+        cse: false,
+        constantOptimizer: false,
+        yul: false,
+      },
 };
