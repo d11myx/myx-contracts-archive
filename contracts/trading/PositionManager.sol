10be346c3540a4e7a7d613d54c0ece175b3c4f89
defiiron <defiiron@gmail.com>
2023-08-11 18:27:19 +0800

    Format code

--- contracts/trading/PositionManager.sol
+++ contracts/trading/PositionManager.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.17;
 
 import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
-import "@openzeppelin/contracts/security/Pausable.sol";
+import '@openzeppelin/contracts/security/Pausable.sol';
 import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import '@openzeppelin/contracts/utils/math/Math.sol';
 
