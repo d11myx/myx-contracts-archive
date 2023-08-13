cc49a29f66b8d0cf96fbfadcc0392220107c8dd1
defiiron <defiiron@gmail.com>
2023-08-13 18:57:04 +0800

    Move to pool

--- contracts/pool/PairToken.sol
+++ contracts/pool/PairToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
-import './interfaces/IPairToken.sol';
+import '../interfaces/IPairToken.sol';
 
 contract PairToken is IPairToken, ERC20, Ownable {
     address public token0;
