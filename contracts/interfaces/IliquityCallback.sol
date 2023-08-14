52ad23514766c7fe0ac779c433f385c7d70e62c3
defiiron <defiiron@gmail.com>
2023-08-14 21:12:15 +0800

    _removeLiquidity

--- contracts/interfaces/IliquityCallback.sol
+++ contracts/interfaces/IliquityCallback.sol
@@ -2,4 +2,6 @@ pragma solidity >=0.8.0;
 
 interface IliquityCallback {
     function addLiquityCallback(uint256 amountIndex, uint256 amountStable, bytes calldata data) external;
+
+    function removeLiquityCallback(address pairToken,uint256 amount, bytes calldata data) external;
 }
