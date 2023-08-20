05717b68d24ba8307e3aa2c604b23abf7dfd65c8
defiiron <defiiron@gmail.com>
2023-08-20 15:14:06 +0800

    Test gas

---
+++ test/test.gas.ts
@@ -0,0 +1,26 @@
+import { ethers, waffle } from 'hardhat';
+import { expect } from './shared/expect';
+
+import snapshotGasCost from './shared/snapshotGasCost';
+import { TestGas, TestMulticall } from '../types';
+
+describe('Multicall', async () => {
+    const wallets = waffle.provider.getWallets();
+
+    let testGas: TestGas ;
+
+    beforeEach('create multicall', async () => {
+        const testGasFacory = await ethers.getContractFactory('TestGas');
+        testGas = (await testGasFacory.deploy()) as TestGas;
+    });
+
+
+
+    it('gas cost of uint256', async () => {
+        await snapshotGasCost(testGas.testKey("1"));
+    });
+
+    it('gas cost of maping(address=>uint256)', async () => {
+        await snapshotGasCost(testGas.testKeys("1"));
+    });
+});
