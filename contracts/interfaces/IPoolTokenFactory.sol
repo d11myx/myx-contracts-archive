f971451836a6a9a1872ad5b23ead47db1c2010f1
defiiron <defiiron@gmail.com>
2023-08-23 16:44:31 +0800

    Remove getPoolToken

--- contracts/interfaces/IPoolTokenFactory.sol
+++ contracts/interfaces/IPoolTokenFactory.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.8.0;
 
 interface IPoolTokenFactory {
-    function getPoolToken(address indexToken, address stableToken) external view returns(address);
+    //function getPoolToken(address indexToken, address stableToken) external view returns(address);
 
     function createPoolToken(address indexToken, address stableToken) external returns (address);
 }
