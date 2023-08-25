42a6e89564e0137e82d3994c7514e208c854a83e
marvin <marvin>
2023-08-25 15:20:17 +0800

    feat: precision to 1e8

--- contracts/libraries/PrecisionUtils.sol
+++ contracts/libraries/PrecisionUtils.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.0;
 import '@openzeppelin/contracts/utils/math/Math.sol';
 
 library PrecisionUtils {
-    uint256 public constant ONE_HUNDRED_PERCENTAGE = 10000;
-    uint256 public constant FUNDING_RATE_PRECISION = 1000000;
+    uint256 public constant PERCENTAGE = 1e8;
     uint256 public constant PRICE_PRECISION = 1e30;
 
     function mulPrice(uint256 amount, uint256 price) internal pure returns (uint256) {
@@ -21,23 +20,23 @@ library PrecisionUtils {
     }
 
     function mulPercentage(uint256 amount, uint256 _percentage) internal pure returns (uint256) {
-        return Math.mulDiv(amount, _percentage, ONE_HUNDRED_PERCENTAGE);
+        return Math.mulDiv(amount, _percentage, PERCENTAGE);
     }
 
     function divPercentage(uint256 amount, uint256 _percentage) internal pure returns (uint256) {
-        return Math.mulDiv(amount, ONE_HUNDRED_PERCENTAGE, _percentage);
+        return Math.mulDiv(amount, PERCENTAGE, _percentage);
     }
 
     function calculatePercentage(uint256 amount0, uint256 amount1) internal pure returns (uint256) {
-        return Math.mulDiv(amount0, ONE_HUNDRED_PERCENTAGE, amount1);
+        return Math.mulDiv(amount0, PERCENTAGE, amount1);
     }
 
     function percentage() internal pure returns (uint256) {
-        return ONE_HUNDRED_PERCENTAGE;
+        return PERCENTAGE;
     }
 
     function fundingRatePrecision() internal pure returns (uint256) {
-        return FUNDING_RATE_PRECISION;
+        return PERCENTAGE;
     }
 
     function pricePrecision() internal pure returns (uint256) {
--- markets/usdt/pairs.ts
+++ markets/usdt/pairs.ts
@@ -10,35 +10,35 @@ export const btcPairInfo: PairInfoConfig = {
         pairToken: ZERO_ADDRESS,
         enable: true,
         kOfSwap: ethers.utils.parseUnits('1', 50),
-        expectIndexTokenP: 5000,
-        addLpFeeP: 100,
-        lpFeeDistributeP: 10000,
+        expectIndexTokenP: 50000000, //50%
+        addLpFeeP: 1000000, //1%
+        lpFeeDistributeP: 100000000,
     },
     tradingConfig: {
-        minLeverage: 0,
-        maxLeverage: 100,
-        minTradeAmount: '100000000000000000',
-        maxTradeAmount: '100000000000000000000000',
-        maxPositionAmount: '100000000000000000000000000',
-        maintainMarginRate: 100,
-        priceSlipP: 5,
-        maxPriceDeviationP: 50,
+        minLeverage: 3,
+        maxLeverage: 50,
+        minTradeAmount: '100000000000000000', //0.1
+        maxTradeAmount: '100000000000000000000000', //100000
+        maxPositionAmount: '100000000000000000000000000', //100000000
+        maintainMarginRate: 1000000, //0.01%
+        priceSlipP: 50000, //0.05%
+        maxPriceDeviationP: 500000, //0.5%
     },
     tradingFeeConfig: {
-        takerFeeP: 30,
-        makerFeeP: 10,
-        lpFeeDistributeP: 3000,
-        keeperFeeDistributeP: 2000,
-        stakingFeeDistributeP: 1000,
+        takerFeeP: 80000, //0.08%
+        makerFeeP: 50000, //0.05%
+        lpFeeDistributeP: 30000000, //30%
+        keeperFeeDistributeP: 20000000, //20%
+        stakingFeeDistributeP: 10000000, //10%
     },
     fundingFeeConfig: {
         minFundingRate: 0,
         maxFundingRate: 0,
-        defaultFundingRate: 100,
-        fundingWeightFactor: 100,
-        liquidityPremiumFactor: 10000,
+        defaultFundingRate: 1000000, //1%
+        fundingWeightFactor: 1000000, //1%
+        liquidityPremiumFactor: 100000000, //100%
         interest: 0,
-        lpDistributeP: 5000,
+        lpDistributeP: 50000000, //50%
     },
 };
 
@@ -50,34 +50,34 @@ export const ethPairInfo: PairInfoConfig = {
         pairToken: ZERO_ADDRESS,
         enable: true,
         kOfSwap: ethers.utils.parseUnits('1', 50),
-        expectIndexTokenP: 5000,
-        addLpFeeP: 100,
-        lpFeeDistributeP: 10000,
+        expectIndexTokenP: 50000000, //50%
+        addLpFeeP: 1000000, //1%
+        lpFeeDistributeP: 100000000,
     },
     tradingConfig: {
-        minLeverage: 0,
-        maxLeverage: 100,
-        minTradeAmount: '100000000000000000',
-        maxTradeAmount: '100000000000000000000000',
-        maxPositionAmount: '100000000000000000000000000',
-        maintainMarginRate: 100,
-        priceSlipP: 5,
-        maxPriceDeviationP: 50,
+        minLeverage: 3,
+        maxLeverage: 50,
+        minTradeAmount: '100000000000000000', //0.1
+        maxTradeAmount: '100000000000000000000000', //100000
+        maxPositionAmount: '100000000000000000000000000', //100000000
+        maintainMarginRate: 1000000, //0.01%
+        priceSlipP: 50000, //0.05%
+        maxPriceDeviationP: 500000, //0.5%
     },
     tradingFeeConfig: {
-        takerFeeP: 30, // 0.3%
-        makerFeeP: 10,
-        lpFeeDistributeP: 3000,
-        keeperFeeDistributeP: 2000,
-        stakingFeeDistributeP: 1000,
+        takerFeeP: 80000, //0.08%
+        makerFeeP: 50000, //0.05%
+        lpFeeDistributeP: 30000000, //30%
+        keeperFeeDistributeP: 20000000, //20%
+        stakingFeeDistributeP: 10000000, //10%
     },
     fundingFeeConfig: {
         minFundingRate: 0,
         maxFundingRate: 0,
-        defaultFundingRate: 100,
-        fundingWeightFactor: 100,
-        liquidityPremiumFactor: 10000,
+        defaultFundingRate: 1000000, //1%
+        fundingWeightFactor: 1000000, //1%
+        liquidityPremiumFactor: 100000000, //100%
         interest: 0,
-        lpDistributeP: 5000,
+        lpDistributeP: 50000000, //50%
     },
 };
