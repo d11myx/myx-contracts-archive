d5b27670a8d76802ae30c49953f57ebf413708bc
defiiron <defiiron@gmail.com>
2023-08-26 10:38:11 +0800

    Use test gas

--- contracts/mock/TestGas.sol
+++ contracts/mock/TestGas.sol
@@ -1,8 +1,11 @@
 pragma solidity 0.8.17;
 
+import '../libraries/TradingTypes.sol';
+
 contract TestGas {
     uint256 public key;
     mapping(address => uint256) keys;
+    mapping(address => TradingTypes.IncreasePositionRequest) keyStructs;
 
     address public owner;
 
@@ -12,5 +15,17 @@ contract TestGas {
 
     function testKeys(uint256 i) external {
         keys[owner] = i;
+    }
+
+    function saveStruct() external {
+        keyStructs[owner] = TradingTypes.IncreasePositionRequest({
+            account: msg.sender,
+            pairIndex: 1,
+            tradeType: TradingTypes.TradeType.LIMIT,
+            collateral: 1,
+            openPrice: 3000,
+            isLong: true,
+            sizeAmount: 1000
+        });
     }
 }
