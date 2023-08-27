ee3d73a4af5f75bbafce4efc83541a46bfef1599
marvin <marvin>
2023-08-27 18:07:16 +0800

    fix: trading fee

--- contracts/interfaces/IFeeManager.sol
+++ contracts/interfaces/IFeeManager.sol
@@ -4,6 +4,8 @@ interface IFeeManager {
     event DistributeTradingFee(
         address account,
         uint256 pairIndex,
+        uint256 tradingFee,
+        uint256 referenceAmount,
         uint256 lpAmount,
         uint256 keeperAmount,
         uint256 stakingAmount,
@@ -16,9 +18,13 @@ interface IFeeManager {
 
     function userTradingFee(address _token, address _account) external view returns (uint256);
 
+    function referenceTradingFee(address _token) external view returns (uint256);
+
     function claimStakingTradingFee(address claimToken) external returns (uint256);
 
     function claimDistributorTradingFee(address claimToken) external returns (uint256);
 
     function claimKeeperTradingFee(address claimToken) external returns (uint256);
+
+    function claimUserTradingFee(address claimToken) external returns (uint256);
 }
