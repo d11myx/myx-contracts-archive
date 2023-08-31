e751cf846d2f4b208167d5257873c83fe8e7bfee
defiiron <defiiron@gmail.com>
2023-08-31 19:14:03 +0800

    Format code

--- contracts/token/RaMYX.sol
+++ contracts/token/RaMYX.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-import "./BaseToken.sol";
+import './BaseToken.sol';
 
 contract RaMYX is BaseToken {
-
-    constructor() ERC20("Raw MYX", "raMYX") {}
-
+    constructor() ERC20('Raw MYX', 'raMYX') {}
 }
