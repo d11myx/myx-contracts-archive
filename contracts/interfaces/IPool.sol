f467a97daa20adb7e90707c20ed97aaca3fcdc65
marvin <marvin@crus.xyz>
2023-09-01 17:25:57 +0800

    feat: pool::getMintLpAmount

--- contracts/interfaces/IPool.sol
+++ contracts/interfaces/IPool.sol
@@ -177,4 +177,11 @@ interface IPool {
         uint256 _minOut,
         bytes calldata data
     ) external returns (uint256 amountIn, uint256 amountOut);
+
+    function getMintLpAmount(
+        uint256 _pairIndex,
+        uint256 _indexAmount,
+        uint256 _stableAmount
+    ) external view returns (uint256 mintAmount, address slipToken, uint256 slipAmount, uint256 indexFeeAmount,
+        uint256 stableFeeAmount, uint256 afterFeeIndexAmount, uint256 afterFeeStableAmount);
 }
