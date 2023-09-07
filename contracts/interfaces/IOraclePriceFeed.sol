d9083cfc27682f397ba55671a904d51d0696f082
defiiron <defiiron@gmail.com>
2023-09-07 17:11:52 +0800

    initTokenConfig

--- contracts/interfaces/IOraclePriceFeed.sol
+++ contracts/interfaces/IOraclePriceFeed.sol
@@ -3,6 +3,8 @@
 pragma solidity 0.8.20;
 
 interface IOraclePriceFeed {
+    event SetToken(address indexed toekn, address indexed priceFeed, uint256 priceDecimals);
+
     function getPrice(address _token) external view returns (uint256);
 
     // function getIndexPrice(address _token, uint256 _referencePrice) external view returns (uint256);
