9f17d082ca728beab323956ffa6c751305102cc0
defiiron <defiiron@gmail.com>
2023-09-08 20:01:02 +0800

    Upgrade

--- test/timelock.spec.ts
+++ test/timelock.spec.ts
@@ -1,5 +1,5 @@
 import { testEnv } from './helpers/make-suite';
-import { Duration, increase, increaseTo, waitForTx } from '../helpers/utilities/tx';
+import { Duration, increase, increaseTo, latest, waitForTx } from '../helpers/utilities/tx';
 import { loadReserveConfig } from '../helpers/market-config-helper';
 import { expect } from './shared/expect';
 import { IPool, TestOwnableToken, Timelock } from '../types';
@@ -28,7 +28,7 @@ describe('Timelock', () => {
 
         timelock = (await deployContract('Timelock', ['43200'])) as Timelock;
         testToken = (await deployContract('TestOwnableToken', [])) as TestOwnableToken;
-        let timestamp = getCurrentTimestamp();
+        let timestamp = await latest();
         let eta = Duration.days(3);
         await timelock.queueTransaction(
             timelock.address,
@@ -51,47 +51,70 @@ describe('Timelock', () => {
     });
 
     it('should not allow non-owner to do operation', async () => {
-        const { deployer, pool, usdt } = testEnv;
+        const {
+            deployer,
+            pool,
+            usdt,
+            users: [depositor, carol, alice],
+        } = testEnv;
         expect(await timelock.pendingAdmin()).to.be.eq(ZERO_ADDRESS);
         expect(await timelock.admin()).to.be.eq(deployer.address);
         await testToken.transferOwnership(timelock.address);
-        // await expect(testToken.transferOwnership(carol, { from: alice }), 'Ownable: caller is not the owner');
-        // await expectRevert(testToken.transferOwnership(carol, { from: bob }), 'Ownable: caller is not the owner');
-        // await expectRevert(
-        //     timelock.queueTransaction(
-        //         testToken.address,
-        //         '0',
-        //         'transferOwnership(address)',
-        //         encodeParameters(['address'], [carol]),
-        //         (await time.latest()).add(time.duration.days(4)),
-        //         { from: alice },
-        //     ),
-        //     'Timelock::queueTransaction: Call must come from admin.',
-        // );
+        await expect(testToken.transferOwnership(carol.address)).to.be.revertedWith('Ownable: caller is not the owner');
+        let timestamp = await latest();
+        let eta = Duration.days(4);
+        await expect(
+            timelock
+                .connect(alice.signer)
+                .queueTransaction(
+                    testToken.address,
+                    '0',
+                    'transferOwnership(address)',
+                    encodeParameters(['address'], [carol.address]),
+                    timestamp.add(eta),
+                ),
+        ).to.be.revertedWith('queueTransaction: Call must come from admin.');
     });
 
-    // it('should do the timelock thing', async () => {
-    //     await testToken.transferOwnership(timelock.address, {from: alice});
-    //     const eta = (await time.latest()).add(time.duration.days(4));
-    //     await timelock.queueTransaction(
-    //         testToken.address, '0', 'transferOwnership(address)',
-    //         encodeParameters(['address'], [carol]), eta, {from: bob},
-    //     );
-    //     await time.increase(time.duration.days(1));
-    //     await expectRevert(
-    //         timelock.executeTransaction(
-    //             testToken.address, '0', 'transferOwnership(address)',
-    //             encodeParameters(['address'], [carol]), eta, {from: bob},
-    //         ),
-    //         "Timelock::executeTransaction: Transaction hasn't surpassed time lock.",
-    //     );
-    //     await time.increase(time.duration.days(4));
-    //     await timelock.executeTransaction(
-    //         testToken.address, '0', 'transferOwnership(address)',
-    //         encodeParameters(['address'], [carol]), eta, {from: bob},
-    //     );
-    //     assert.equal((await testToken.owner()).valueOf(), carol);
-    // });
+    it('should do the timelock thing', async () => {
+        const {
+            deployer,
+            pool,
+            usdt,
+            users: [depositor, carol, alice],
+        } = testEnv;
+        await testToken.transferOwnership(timelock.address);
+        let timestamp = await latest();
+        let eta = Duration.days(4);
+        console.log('timestamp:' + timestamp);
+        console.log('eta:' + eta);
+        await timelock.queueTransaction(
+            testToken.address,
+            '0',
+            'transferOwnership(address)',
+            encodeParameters(['address'], [carol.address]),
+            timestamp.add(eta),
+        );
+        await increase(Duration.days(1));
+        await expect(
+            timelock.executeTransaction(
+                testToken.address,
+                '0',
+                'transferOwnership(address)',
+                encodeParameters(['address'], [carol.address]),
+                timestamp.add(eta),
+            ),
+        ).to.be.revertedWith("Transaction hasn't surpassed time lock.");
+        await increase(Duration.days(4));
+        await timelock.executeTransaction(
+            testToken.address,
+            '0',
+            'transferOwnership(address)',
+            encodeParameters(['address'], [carol.address]),
+            timestamp.add(eta),
+        );
+        expect(await testToken.owner()).to.be.eq(carol.address);
+    });
     // it('test cancelTransaction', async () => {
     //     assert.equal(await testToken.owner().valueOf(), alice);
     //     await testToken.transferOwnership(timelock.address, {from: alice});
