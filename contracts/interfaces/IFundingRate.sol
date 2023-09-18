5ba318c69252c8d77e7353167e2bac4b860cddb6
defiiron <defiiron@gmail.com>
2023-09-18 14:00:55 +0800

    Use FundingFeeConfig

--- contracts/interfaces/IFundingRate.sol
+++ contracts/interfaces/IFundingRate.sol
@@ -1,5 +1,13 @@
 pragma solidity ^0.8.0;
 
 interface IFundingRate {
+
+     struct FundingFeeConfig {
+        int256 minFundingRate; // Minimum capital rate 1e8 for 100%
+        int256 maxFundingRate; // The maximum capital rate is 1e8 for 100%
+        uint256 fundingWeightFactor; // The weight coefficient of the fund rate of both sides is  for 100%
+        uint256 liquidityPremiumFactor; // The coefficient of liquidity to premium is 1e8 for 100%
+        int256 interest;
+    }
     // function getFundingRate(uint256 _pairIndex) external view returns (uint256);
 }
