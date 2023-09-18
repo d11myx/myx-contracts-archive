c8a48b6b4be7181fd46ae8dfcdee4aebf398afa6
defiiron <defiiron@gmail.com>
2023-09-18 13:52:19 +0800

    Upgrade fundingRate

--- contracts/core/FundingRate.sol
+++ contracts/core/FundingRate.sol
@@ -1,12 +1,25 @@
 pragma solidity ^0.8.0;
 
-import "../interfaces/IFundingRate.sol";
-import "../interfaces/IPool.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract FundingRate is IFundingRate {
+import "../interfaces/IFundingRate.sol";
+import "../interfaces/IOraclePriceFeed.sol";
+import "../interfaces/IPool.sol";
+import "../libraries/PrecisionUtils.sol";
+import "../libraries/Roleable.sol";
+import "../libraries/Int256Utils.sol";
+
+contract FundingRate is IFundingRate, Roleable {
+    using PrecisionUtils for uint256;
+    using Int256Utils for int256;
+    using Math for uint256;
+    using SafeMath for uint256;
+
     IPool public immutable pool;
 
-    constructor(IPool _pool) {
+    uint256 public fundingInterval;
+
+    constructor(IAddressesProvider addressProvider, IPool _pool) Roleable(addressProvider) {
         pool = _pool;
     }
 
@@ -15,14 +28,15 @@ contract FundingRate is IFundingRate {
         int256 currentExposureAmountChecker,
         uint256 longTracker,
         uint256 shortTracker
-
-    ) public view  returns (uint256 fundingRate) {
+    ) public view returns (int256 fundingRate) {
+        IPool.Pair memory pair = pool.getPair(_pairIndex);
         IPool.FundingFeeConfig memory fundingFeeConfig = pool.getFundingFeeConfig(_pairIndex);
         int256 w = int256(fundingFeeConfig.fundingWeightFactor);
-        int256 q = int256(longTracker[_pairIndex] + shortTracker[_pairIndex]);
+        int256 q = int256(longTracker + shortTracker);
         int256 k = int256(fundingFeeConfig.liquidityPremiumFactor);
 
         IPool.Vault memory lpVault = pool.getVault(_pairIndex);
+        uint256 _price = IOraclePriceFeed(ADDRESS_PROVIDER.priceOracle()).getPrice(pair.indexToken);
         int256 l = int256(
             (lpVault.indexTotalAmount - lpVault.indexReservedAmount).mulPrice(_price) +
                 (lpVault.stableTotalAmount - lpVault.stableReservedAmount)
@@ -31,17 +45,20 @@ contract FundingRate is IFundingRate {
         if (q == 0) {
             fundingRate = 0;
         } else {
-            fundingRate = (w * currentExposureAmountChecker * int256(PrecisionUtils.fundingRatePrecision())) / (k * q);
+            fundingRate =
+                (w * currentExposureAmountChecker * int256(PrecisionUtils.fundingRatePrecision())) /
+                (k * q);
             if (l != 0) {
                 fundingRate =
                     fundingRate +
-                    ((int256(PrecisionUtils.fundingRatePrecision()) - w) * currentExposureAmountChecker) /
+                    ((int256(PrecisionUtils.fundingRatePrecision()) - w) *
+                        currentExposureAmountChecker) /
                     (k * l);
             }
         }
-        fundingRate = (fundingRate - fundingFeeConfig.interest).max(fundingFeeConfig.minFundingRate).min(
-            fundingFeeConfig.maxFundingRate
-        );
+        fundingRate = (fundingRate - fundingFeeConfig.interest)
+            .max(fundingFeeConfig.minFundingRate)
+            .min(fundingFeeConfig.maxFundingRate);
         fundingRate = fundingRate / int256(365) / int256(86400 / fundingInterval);
     }
 }
