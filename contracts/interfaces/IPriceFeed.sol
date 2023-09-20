461475269f7c16955a8b9e0cfbfa1194d880aba6
marvin <marvin@crus.xyz>
2023-09-20 13:52:48 +0800

    fix: price oracle

--- contracts/interfaces/IPriceFeed.sol
+++ contracts/interfaces/IPriceFeed.sol
@@ -3,15 +3,8 @@ pragma solidity ^0.8.0;
 
 interface IPriceFeed {
 
-    function updatePrice(address[] calldata tokens, uint256[] calldata prices) external payable;
-
     function getPrice(address token) external view returns (uint256);
 
     function decimals() external view returns (uint256);
-
-    function getUpdateFee(
-        address[] calldata tokens,
-        uint256[] calldata prices
-    ) external view returns (uint);
 
 }
