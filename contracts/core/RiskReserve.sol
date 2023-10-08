ef204bf81f602396f9cd07ae5c6969357ad960a9
marvin <marvin>
2023-10-08 16:48:06 +0800

    risk reserve

--- contracts/core/RiskReserve.sol
+++ contracts/core/RiskReserve.sol
@@ -4,9 +4,10 @@ pragma solidity 0.8.20;
 import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "../interfaces/IAddressesProvider.sol";
+import "../interfaces/IRiskReserve.sol";
 import "../libraries/Roleable.sol";
 
-contract RiskReserve is Roleable {
+contract RiskReserve is IRiskReserve, Roleable {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
@@ -14,25 +15,6 @@ contract RiskReserve is Roleable {
 
     address public addressDao;
     address public addressPositionManager;
-
-    event UpdatedDaoAddress(
-        address sender,
-        address oldAddress,
-        address newAddress
-    );
-
-    event UpdatedPositionManagerAddress(
-        address sender,
-        address oldAddress,
-        address newAddress
-    );
-
-    event Withdraw(
-        address sender,
-        address asset,
-        uint256 amount,
-        address to
-    );
 
     constructor(
         address _addressDao,
@@ -42,42 +24,41 @@ contract RiskReserve is Roleable {
     }
 
     modifier onlyDao() {
-        require(msg.sender == addressDao, 'forbidden');
+        require(msg.sender == addressDao, 'onlyDao');
         _;
     }
 
     modifier onlyPositionManager() {
-        require(msg.sender == addressPositionManager, 'forbidden');
+        require(msg.sender == addressPositionManager, 'onlyPositionManager');
         _;
     }
 
-    function updateDaoAddress(address newAddress) external onlyPoolAdmin {
+    function updateDaoAddress(address newAddress) external override onlyPoolAdmin {
         address oldAddress = addressDao;
         addressDao = newAddress;
         emit UpdatedDaoAddress(msg.sender, oldAddress, addressDao);
     }
 
-    function updatePositionManagerAddress(address newAddress) external onlyPoolAdmin {
+    function updatePositionManagerAddress(address newAddress) external override onlyPoolAdmin {
         address oldAddress = addressDao;
         addressPositionManager = newAddress;
         emit UpdatedPositionManagerAddress(msg.sender, oldAddress, addressPositionManager);
     }
 
-    function recharge(address asset, uint256 amount) external {
+    function increase(address asset, uint256 amount) external override onlyPositionManager {
+        assetReservedAmount[asset] += int256(amount);
+    }
+
+    function decrease(address asset, uint256 amount) external override onlyPositionManager {
+        assetReservedAmount[asset] -= int256(amount);
+    }
+
+    function recharge(address asset, uint256 amount) external override {
         IERC20(asset).safeTransferFrom(msg.sender, address(this), amount);
         assetReservedAmount[asset] += int256(amount);
     }
 
-    function increase(address asset, uint256 amount) external onlyPositionManager {
-        assetReservedAmount[asset] += int256(amount);
-    }
-
-    function decrease(address asset, uint256 amount) external onlyPositionManager {
-        require(int256(amount) <= assetReservedAmount[asset], 'insufficient reserved amount');
-        assetReservedAmount[asset] -= int256(amount);
-    }
-
-    function withdraw(address asset, uint256 amount, address to) external onlyDao {
+    function withdraw(address asset, uint256 amount, address to) external override onlyDao {
         require(int256(amount) <= assetReservedAmount[asset], 'insufficient reserved amount');
         require(amount <= IERC20(asset).balanceOf(address(this)), 'insufficient balance');
 
@@ -87,7 +68,7 @@ contract RiskReserve is Roleable {
         emit Withdraw(msg.sender, asset, amount, to);
     }
 
-    function rescue(address asset, address to) external onlyPoolAdmin {
+    function rescue(address asset, address to) external override onlyPoolAdmin {
         IERC20(asset).safeTransfer(to, IERC20(asset).balanceOf(address(this)));
     }
 }
