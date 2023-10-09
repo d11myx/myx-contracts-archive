fe3054e19c276256b29069326511f87162874c86
defiiron <defiiron@gmail.com>
2023-10-09 15:13:31 +0800

    Use Upgradeable

--- contracts/libraries/Upgradeable.sol
+++ contracts/libraries/Upgradeable.sol
@@ -4,18 +4,21 @@ pragma solidity 0.8.20;
 import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 
-contract Upgradeable is Initializable, UUPSUpgradeable {
-    constructor() initializer {}
+import "../interfaces/IAddressesProvider.sol";
 
-    function initialize() public initializer {
-        // __Ownable_init();
-        // __UUPSUpgradeable_init();
+contract Upgradeable is Initializable, UUPSUpgradeable {
+    IAddressesProvider public immutable ADDRESS_PROVIDER;
+
+    constructor(IAddressesProvider addressProvider) initializer {
+        ADDRESS_PROVIDER = addressProvider;
     }
 
+    // function initialize() public initializer {
+    //     // __Ownable_init();
+    //     // __UUPSUpgradeable_init();
+    // }
+
     function _authorizeUpgrade(address newImplementation) internal virtual override {
-        //todo add hasRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        // require(_msgSender() == owner(), "Unauthorized access");
-        // console.log("_authorizeUpgrade executed for admin:", _msgSender());
-        // console.log("New implementation address:", newImplementation);
+        require(msg.sender == ADDRESS_PROVIDER.timelock(), "Unauthorized access");
     }
 }
