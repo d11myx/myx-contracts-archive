637b01627ced4f3951e6181f98937d73972213bf
defiiron <defiiron@gmail.com>
2023-10-09 15:32:04 +0800

    Use hardhat

--- contracts/libraries/Roleable.sol
+++ contracts/libraries/Roleable.sol
@@ -1,26 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import '../interfaces/IAddressesProvider.sol';
-import '../interfaces/IRoleManager.sol';
+import "../interfaces/IAddressesProvider.sol";
+import "../interfaces/IRoleManager.sol";
 
-abstract contract Roleable {
+import "../libraries/Upgradeable.sol";
+
+abstract contract Roleable is Upgradeable {
     mapping(address => bool) public isHandler;
 
-    IAddressesProvider public immutable ADDRESS_PROVIDER;
+    // IAddressesProvider public immutable ADDRESS_PROVIDER;
 
-    constructor(IAddressesProvider addressProvider) {
+    constructor(IAddressesProvider addressProvider) Upgradeable(addressProvider) {
         isHandler[msg.sender] = true;
-        ADDRESS_PROVIDER = addressProvider;
+        // ADDRESS_PROVIDER = addressProvider;
     }
 
     modifier onlyAdmin() {
-        require(IRoleManager(ADDRESS_PROVIDER.roleManager()).isAdmin(msg.sender), 'onlyAdmin');
+        require(IRoleManager(ADDRESS_PROVIDER.roleManager()).isAdmin(msg.sender), "onlyAdmin");
         _;
     }
 
     modifier onlyPoolAdmin() {
-        require(IRoleManager(ADDRESS_PROVIDER.roleManager()).isPoolAdmin(msg.sender), 'onlyPoolAdmin');
+        require(
+            IRoleManager(ADDRESS_PROVIDER.roleManager()).isPoolAdmin(msg.sender),
+            "onlyPoolAdmin"
+        );
         _;
     }
 }
