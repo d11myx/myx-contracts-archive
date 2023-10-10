8d5b6b48364cb6a49b238430e48f068adf37a2e3
defiiron <defiiron@gmail.com>
2023-10-10 14:15:35 +0800

    Upgrade Upgradeable

--- contracts/libraries/Roleable.sol
+++ contracts/libraries/Roleable.sol
@@ -6,16 +6,8 @@ import "../interfaces/IRoleManager.sol";
 
 import "../libraries/Upgradeable.sol";
 
-abstract contract Roleable is Upgradeable {
-    mapping(address => bool) public isHandler;
-
-    // IAddressesProvider public immutable ADDRESS_PROVIDER;
-
-    constructor(IAddressesProvider addressProvider) Upgradeable(addressProvider) {
-        isHandler[msg.sender] = true;
-        // ADDRESS_PROVIDER = addressProvider;
-    }
-
+abstract contract Roleable {
+    IAddressesProvider public ADDRESS_PROVIDER;
     modifier onlyAdmin() {
         require(IRoleManager(ADDRESS_PROVIDER.roleManager()).isAdmin(msg.sender), "onlyAdmin");
         _;
