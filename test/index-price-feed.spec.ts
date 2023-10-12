34619c7e32f217686b8bd3dc3484a1143d515f1a
defiiron <defiiron@gmail.com>
2023-10-12 14:09:12 +0800

    Rename

--- test/index-price-feed.spec.ts
+++ test/index-price-feed.spec.ts
@@ -36,7 +36,7 @@
 //   let fastPriceFeed
 
 //   beforeEach(async () => {
-//     vaultPriceFeed = await deployContract("OraclePriceFeed", [])
+//     vaultPriceFeed = await deployContract("PythOraclePriceFeed", [])
 
 //     bnb = await deployContract("Token", [])
 //     bnbPriceFeed = await deployContract("PriceFeed", [])
