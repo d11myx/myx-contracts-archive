f1b555d0818f00e276c970bf48c1e269efd80f37
defiiron <defiiron@gmail.com>
2023-10-16 14:59:31 +0800

    Usse 30

--- test/helpers/misc.ts
+++ test/helpers/misc.ts
@@ -19,7 +19,7 @@ export async function updateBTCPrice(testEnv: TestEnv, btcPrice: string) {
     );
 
     await waitForTx(
-        await indexPriceFeed.connect(keeper.signer).updatePrice([btc.address], [ethers.utils.parseUnits(btcPrice, 8)]),
+        await indexPriceFeed.connect(keeper.signer).updatePrice([btc.address], [ethers.utils.parseUnits(btcPrice, 30)]),
     );
 }
 
