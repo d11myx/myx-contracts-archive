02665f2e7c51b072a4e8dbffb364dc42207fbfd3
defiiron <defiiron@gmail.com>
2023-10-16 11:53:19 +0800

    Test chainlink

--- contracts/oracle/ChainlinkPriceFeed.sol
+++ contracts/oracle/ChainlinkPriceFeed.sol
@@ -17,6 +17,8 @@ pragma solidity 0.8.20;
 contract ChainlinkPriceFeed is IPriceFeed, Roleable {
     using SafeMath for uint256;
 
+    event FeedUpdate(address asset, address  feed);
+
     uint256 public immutable PRICE_DECIMALS = 30;
     uint256 public constant BASIS_POINTS_DIVISOR = 10000;
     uint256 public constant MAX_SPREAD_BASIS_POINTS = 50;
@@ -31,8 +33,13 @@ contract ChainlinkPriceFeed is IPriceFeed, Roleable {
 
     mapping(address => address) public priceFeeds;
 
-    constructor(IAddressesProvider _addressProvider) {
+    constructor(IAddressesProvider _addressProvider, address[] memory _assets,address[] memory _feeds) {
         ADDRESS_PROVIDER = _addressProvider;
+    }
+
+     modifier onlyTimelock() {
+        require(msg.sender == ADDRESS_PROVIDER.timelock(), "only timelock");
+        _;
     }
 
     function decimals() public pure override returns (uint256) {
@@ -44,10 +51,18 @@ contract ChainlinkPriceFeed is IPriceFeed, Roleable {
     }
 
     function setTokenConfig(
-        address _token,
-        address _priceFeed
-    ) external onlyPoolAdmin {
-        priceFeeds[_token] = _priceFeed;
+      address[] memory assets, address[] memory feeds
+    ) external onlyTimelock {
+       _setAssetPrices(assets,feeds);
+    }
+
+     function _setAssetPrices(address[] memory assets, address[] memory feeds) private {
+        require(assets.length == feeds.length, "inconsistent params length");
+        for (uint256 i = 0; i < assets.length; i++) {
+            require(assets[i]!=address(0)&&feeds[i]!=address(0),"is 0");
+            priceFeeds[assets[i]] = feeds[i];
+            emit FeedUpdate(assets[i], feeds[i]);
+        }
     }
 
     function getPrice(address _token) public view override returns (uint256) {
