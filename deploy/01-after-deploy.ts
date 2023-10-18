5850e371f617a5d897929f7a1ba73ae4d4dbecf2
marvin <marvin>
2023-10-18 20:31:39 +0800

    upgradeTo 0.8.19

--- deploy/01-after-deploy.ts
+++ deploy/01-after-deploy.ts
@@ -1,10 +1,8 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
-import { loadReserveConfig, MARKET_NAME } from '../helpers';
 
 const func: DeployFunction = async function ({ getNamedAccounts, deployments, ...hre }: HardhatRuntimeEnvironment) {
     console.log('=== Post deployment hook ===');
-    const reserveConfig = loadReserveConfig(MARKET_NAME);
 
     await hre.run('print-deployments');
 
