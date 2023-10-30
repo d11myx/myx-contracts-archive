2e48e55aa3f7d94ec4911c9fdec3af5855b7c30f
李志仁 <lizhiren8023@gmail.com>
2023-10-30 21:11:36 +0800

    Add: price diff valid

--- test/position.spec.ts
+++ test/position.spec.ts
@@ -1593,5 +1593,360 @@ describe('Position', () => {
                 );
             });
         });
+
+        describe('oracle price > keeper price 0.5%, exceed max price deviation', () => {
+            before('add liquidity', async () => {
+                testEnv = await newTestEnv();
+                const {
+                    users: [depositor],
+                    usdt,
+                    btc,
+                    pool,
+                    router,
+                } = testEnv;
+
+                // add liquidity
+                const indexAmount = ethers.utils.parseUnits('10000', await btc.decimals());
+                const stableAmount = ethers.utils.parseUnits('300000000', await usdt.decimals());
+                const pair = await pool.getPair(pairIndex);
+                await mintAndApprove(testEnv, btc, indexAmount, depositor, router.address);
+                await mintAndApprove(testEnv, usdt, stableAmount, depositor, router.address);
+
+                await router
+                    .connect(depositor.signer)
+                    .addLiquidity(pair.indexToken, pair.stableToken, indexAmount, stableAmount);
+            });
+
+            it('should cancel liquidate position', async () => {
+                const {
+                    users: [trader],
+                    usdt,
+                    btc,
+                    router,
+                    positionManager,
+                    orderManager,
+                    executionLogic,
+                    liquidationLogic,
+                    keeper,
+                    pool,
+                    oraclePriceFeed,
+                    indexPriceFeed,
+                } = testEnv;
+                const collateral = ethers.utils.parseUnits('300000', await usdt.decimals());
+                const size = ethers.utils.parseUnits('30', await btc.decimals());
+                const openPrice = ethers.utils.parseUnits('30000', 30);
+
+                /**
+                 * trader increase short entrust position
+                 */
+                let shortPositionBefore = await positionManager.getPosition(trader.address, pairIndex, false);
+                expect(shortPositionBefore.positionAmount).to.be.eq('0');
+
+                await mintAndApprove(testEnv, usdt, collateral, trader, router.address);
+                let balance = await usdt.balanceOf(trader.address);
+                expect(balance).to.be.eq(collateral);
+
+                const increseEntrustPositionRequest: TradingTypes.IncreasePositionRequestStruct = {
+                    account: trader.address,
+                    pairIndex,
+                    tradeType: TradeType.MARKET,
+                    collateral: collateral,
+                    openPrice,
+                    isLong: false,
+                    sizeAmount: size,
+                    maxSlippage: 0,
+                };
+
+                const entrustOrderId = await orderManager.ordersIndex();
+                await router.connect(trader.signer).createIncreaseOrder(increseEntrustPositionRequest);
+                const entrustOrderBefore = await orderManager.getIncreaseOrder(entrustOrderId, TradeType.MARKET);
+                shortPositionBefore = await positionManager.getPosition(trader.address, pairIndex, false);
+                balance = await usdt.balanceOf(trader.address);
+
+                expect(entrustOrderBefore.sizeAmount).to.be.eq(size);
+                expect(shortPositionBefore.positionAmount).to.be.eq('0');
+                expect(balance).to.be.eq('0');
+
+                /**
+                 * increase short position
+                 */
+                await mintAndApprove(testEnv, usdt, collateral, trader, router.address);
+                balance = await usdt.balanceOf(trader.address);
+                expect(balance).to.be.eq(collateral);
+
+                const incresePositionRequest: TradingTypes.IncreasePositionRequestStruct = {
+                    account: trader.address,
+                    pairIndex,
+                    tradeType: TradeType.MARKET,
+                    collateral,
+                    openPrice,
+                    isLong: false,
+                    sizeAmount: size,
+                    maxSlippage: 0,
+                };
+
+                const orderId = await orderManager.ordersIndex();
+                await router.connect(trader.signer).createIncreaseOrder(incresePositionRequest);
+                await executionLogic.connect(keeper.signer).executeIncreaseOrder(orderId, TradeType.MARKET, 0, 0);
+                shortPositionBefore = await positionManager.getPosition(trader.address, pairIndex, false);
+                balance = await usdt.balanceOf(trader.address);
+
+                expect(balance).to.be.eq('0');
+                expect(shortPositionBefore.positionAmount).to.be.eq(size);
+
+                // update maintainMarginRate
+                const btcPair = loadReserveConfig(MARKET_NAME).PairsConfig['BTC'];
+                const tradingConfigBefore = btcPair.tradingConfig;
+                tradingConfigBefore.maintainMarginRate = 15000000;
+                await pool.updateTradingConfig(pairIndex, tradingConfigBefore);
+                const tradingConfigAfter = await pool.getTradingConfig(pairIndex);
+
+                expect(tradingConfigAfter.maintainMarginRate).to.be.eq(tradingConfigBefore.maintainMarginRate);
+
+                // update price
+                await updateBTCPrice(testEnv, '35480');
+
+                // calculate pnl、tradingFee
+                const pair = await pool.getPair(pairIndex);
+                const tradingFeeConfig = await pool.getTradingFeeConfig(pairIndex);
+                const tradingConfig = await pool.getTradingConfig(pairIndex);
+                const poolPrice = await pool.getPrice(pair.indexToken);
+                const indexToStableAmount = await convertIndexAmountToStable(
+                    btc,
+                    usdt,
+                    shortPositionBefore.positionAmount,
+                );
+                const pnl = indexToStableAmount
+                    .mul(-1)
+                    .mul(poolPrice.sub(shortPositionBefore.averagePrice))
+                    .div('1000000000000000000000000000000');
+                const sizeDelta = indexToStableAmount.mul(poolPrice).div('1000000000000000000000000000000');
+                const tradingFee = sizeDelta.mul(tradingFeeConfig.takerFeeP).div('100000000');
+
+                // calculate riskRate
+                const exposureAsset = shortPositionBefore.collateral.add(pnl).sub(tradingFee);
+                const margin = shortPositionBefore.positionAmount
+                    .mul(shortPositionBefore.averagePrice)
+                    .div('1000000000000000000000000000000')
+                    .mul(tradingConfig.maintainMarginRate)
+                    .div('100000000');
+                const riskRate = margin.mul('100000000').div(exposureAsset);
+
+                // riskRate = 100%
+                expect(riskRate.div('100000000')).to.be.eq('100');
+
+                // update oracle price
+                const latestOraclePrice = ethers.utils.parseUnits('35659', 8);
+                const updateData = await oraclePriceFeed.getUpdateData([btc.address], [latestOraclePrice]);
+                const mockPyth = await ethers.getContractAt('MockPyth', await oraclePriceFeed.pyth());
+                const fee = mockPyth.getUpdateFee(updateData);
+                await oraclePriceFeed
+                    .connect(keeper.signer)
+                    .updatePrice([btc.address], [latestOraclePrice], { value: fee });
+                const oraclePrice = await oraclePriceFeed.getPrice(btc.address);
+                const indexPrice = await indexPriceFeed.getPrice(btc.address);
+
+                // oraclePrice > indexPrice 0.5%
+                expect(oraclePrice.sub(indexPrice).mul('100000000').div(oraclePrice)).to.be.gte(
+                    tradingConfig.maxPriceDeviationP,
+                );
+
+                // execute liquidatePositions
+                const positionKey = await positionManager.getPositionKey(trader.address, pairIndex, false);
+                await expect(
+                    liquidationLogic
+                        .connect(keeper.signer)
+                        .liquidatePositions([
+                            { positionKey: positionKey, level: 0, commissionRatio: 0, sizeAmount: 0 },
+                        ]),
+                ).to.be.revertedWith('exceed max price deviation');
+            });
+        });
+
+        describe('oracle price < keeper price 0.5%, liquidate position', () => {
+            before('add liquidity', async () => {
+                testEnv = await newTestEnv();
+                const {
+                    users: [depositor],
+                    usdt,
+                    btc,
+                    pool,
+                    router,
+                } = testEnv;
+
+                // add liquidity
+                const indexAmount = ethers.utils.parseUnits('10000', await btc.decimals());
+                const stableAmount = ethers.utils.parseUnits('300000000', await usdt.decimals());
+                const pair = await pool.getPair(pairIndex);
+                await mintAndApprove(testEnv, btc, indexAmount, depositor, router.address);
+                await mintAndApprove(testEnv, usdt, stableAmount, depositor, router.address);
+
+                await router
+                    .connect(depositor.signer)
+                    .addLiquidity(pair.indexToken, pair.stableToken, indexAmount, stableAmount);
+            });
+
+            it('should liquidate position', async () => {
+                const {
+                    users: [trader],
+                    usdt,
+                    btc,
+                    router,
+                    positionManager,
+                    orderManager,
+                    executionLogic,
+                    liquidationLogic,
+                    keeper,
+                    pool,
+                    oraclePriceFeed,
+                    indexPriceFeed,
+                    riskReserve,
+                } = testEnv;
+                const collateral = ethers.utils.parseUnits('300000', await usdt.decimals());
+                const size = ethers.utils.parseUnits('30', await btc.decimals());
+                const openPrice = ethers.utils.parseUnits('30000', 30);
+
+                /**
+                 * trader increase short entrust position
+                 */
+                let shortPositionBefore = await positionManager.getPosition(trader.address, pairIndex, false);
+                expect(shortPositionBefore.positionAmount).to.be.eq('0');
+
+                await mintAndApprove(testEnv, usdt, collateral, trader, router.address);
+                let balance = await usdt.balanceOf(trader.address);
+                expect(balance).to.be.eq(collateral);
+
+                const increseEntrustPositionRequest: TradingTypes.IncreasePositionRequestStruct = {
+                    account: trader.address,
+                    pairIndex,
+                    tradeType: TradeType.MARKET,
+                    collateral: collateral,
+                    openPrice,
+                    isLong: false,
+                    sizeAmount: size,
+                    maxSlippage: 0,
+                };
+
+                const entrustOrderId = await orderManager.ordersIndex();
+                await router.connect(trader.signer).createIncreaseOrder(increseEntrustPositionRequest);
+                const entrustOrderBefore = await orderManager.getIncreaseOrder(entrustOrderId, TradeType.MARKET);
+                shortPositionBefore = await positionManager.getPosition(trader.address, pairIndex, false);
+                balance = await usdt.balanceOf(trader.address);
+
+                expect(entrustOrderBefore.sizeAmount).to.be.eq(size);
+                expect(shortPositionBefore.positionAmount).to.be.eq('0');
+                expect(balance).to.be.eq('0');
+
+                /**
+                 * increase short position
+                 */
+                await mintAndApprove(testEnv, usdt, collateral, trader, router.address);
+                balance = await usdt.balanceOf(trader.address);
+                expect(balance).to.be.eq(collateral);
+
+                const incresePositionRequest: TradingTypes.IncreasePositionRequestStruct = {
+                    account: trader.address,
+                    pairIndex,
+                    tradeType: TradeType.MARKET,
+                    collateral,
+                    openPrice,
+                    isLong: false,
+                    sizeAmount: size,
+                    maxSlippage: 0,
+                };
+
+                const orderId = await orderManager.ordersIndex();
+                await router.connect(trader.signer).createIncreaseOrder(incresePositionRequest);
+                await executionLogic.connect(keeper.signer).executeIncreaseOrder(orderId, TradeType.MARKET, 0, 0);
+                shortPositionBefore = await positionManager.getPosition(trader.address, pairIndex, false);
+                balance = await usdt.balanceOf(trader.address);
+
+                expect(balance).to.be.eq('0');
+                expect(shortPositionBefore.positionAmount).to.be.eq(size);
+
+                // update maintainMarginRate
+                const btcPair = loadReserveConfig(MARKET_NAME).PairsConfig['BTC'];
+                const tradingConfigBefore = btcPair.tradingConfig;
+                tradingConfigBefore.maintainMarginRate = 15000000;
+                await pool.updateTradingConfig(pairIndex, tradingConfigBefore);
+                const tradingConfigAfter = await pool.getTradingConfig(pairIndex);
+
+                expect(tradingConfigAfter.maintainMarginRate).to.be.eq(tradingConfigBefore.maintainMarginRate);
+
+                // update price
+                await updateBTCPrice(testEnv, '35480');
+
+                // calculate pnl、tradingFee
+                const pair = await pool.getPair(pairIndex);
+                const tradingFeeConfig = await pool.getTradingFeeConfig(pairIndex);
+                const tradingConfig = await pool.getTradingConfig(pairIndex);
+                const poolPrice = await pool.getPrice(pair.indexToken);
+                const indexToStableAmount = await convertIndexAmountToStable(
+                    btc,
+                    usdt,
+                    shortPositionBefore.positionAmount,
+                );
+                const pnl = indexToStableAmount
+                    .mul(-1)
+                    .mul(poolPrice.sub(shortPositionBefore.averagePrice))
+                    .div('1000000000000000000000000000000');
+                const sizeDelta = indexToStableAmount.mul(poolPrice).div('1000000000000000000000000000000');
+                const tradingFee = sizeDelta.mul(tradingFeeConfig.takerFeeP).div('100000000');
+
+                // calculate riskRate
+                const exposureAsset = shortPositionBefore.collateral.add(pnl).sub(tradingFee);
+                const margin = shortPositionBefore.positionAmount
+                    .mul(shortPositionBefore.averagePrice)
+                    .div('1000000000000000000000000000000')
+                    .mul(tradingConfig.maintainMarginRate)
+                    .div('100000000');
+                const riskRate = margin.mul('100000000').div(exposureAsset);
+
+                // riskRate = 100%
+                expect(riskRate.div('100000000')).to.be.eq('100');
+
+                // update oracle price
+                const latestOraclePrice = ethers.utils.parseUnits('35621', 8);
+                const updateData = await oraclePriceFeed.getUpdateData([btc.address], [latestOraclePrice]);
+                const mockPyth = await ethers.getContractAt('MockPyth', await oraclePriceFeed.pyth());
+                const fee = mockPyth.getUpdateFee(updateData);
+                await oraclePriceFeed
+                    .connect(keeper.signer)
+                    .updatePrice([btc.address], [latestOraclePrice], { value: fee });
+                const oraclePrice = await oraclePriceFeed.getPrice(btc.address);
+                const indexPrice = await indexPriceFeed.getPrice(btc.address);
+
+                // oraclePrice < indexPrice 0.5%
+                expect(indexPrice.sub(oraclePrice).mul('100000000').div(oraclePrice)).to.be.lt(
+                    tradingConfig.maxPriceDeviationP,
+                );
+
+                // execute liquidatePositions
+                const positionKey = await positionManager.getPositionKey(trader.address, pairIndex, false);
+                await liquidationLogic
+                    .connect(keeper.signer)
+                    .liquidatePositions([{ positionKey: positionKey, level: 0, commissionRatio: 0, sizeAmount: 0 }]);
+
+                balance = await usdt.balanceOf(trader.address);
+                const reserveBalance = await riskReserve.getReservedAmount(usdt.address);
+                const shortPositionAfter = await positionManager.getPosition(trader.address, pairIndex, false);
+                const entrustOrderAfter = await orderManager.getIncreaseOrder(entrustOrderId, TradeType.MARKET);
+
+                // calculate totalSettlementAmount
+                const poolPriceAfter = await pool.getPrice(pair.indexToken);
+                const pnlAfter = indexToStableAmount
+                    .mul(-1)
+                    .mul(poolPriceAfter.sub(shortPositionBefore.averagePrice))
+                    .div('1000000000000000000000000000000');
+                const sizeDeltaAfter = indexToStableAmount.mul(poolPriceAfter).div('1000000000000000000000000000000');
+                const tradingFeeAfter = sizeDeltaAfter.mul(tradingFeeConfig.takerFeeP).div('100000000');
+                const totalSettlementAmount = pnlAfter.sub(tradingFeeAfter);
+
+                expect(balance).to.be.eq(entrustOrderBefore.collateral);
+                expect(reserveBalance).to.be.eq(shortPositionBefore.collateral.sub(totalSettlementAmount.abs()));
+                expect(shortPositionAfter.positionAmount).to.be.eq('0');
+                expect(entrustOrderAfter.sizeAmount).to.be.eq('0');
+            });
+        });
     });
 });
