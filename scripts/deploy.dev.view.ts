df0bba388adbde7de027338016f8331552e4fdd5
marvin <marvin>
2023-10-31 14:23:38 +0800

    fix

--- scripts/deploy.dev.view.ts
+++ scripts/deploy.dev.view.ts
@@ -1,6 +1,7 @@
 // @ts-ignore
-import { ethers } from 'hardhat';
+import hre, { ethers } from 'hardhat';
 import { getOraclePriceFeed, getPool, getTokens } from '../helpers';
+import type { SignerWithAddress } from '@nomiclabs/hardhat-ethers/src/signers';
 
 async function main() {
     const [deployer] = await ethers.getSigners();
@@ -25,6 +26,18 @@ async function main() {
     console.log(`eth price:`, ethOraclePrice);
     console.log(`btc price:`, btcIndexPrice);
     console.log(`eth price:`, ethIndexPrice);
+
+    // curl -X POST --data '{"jsonrpc":"2.0","method":"hardhat_setBalance","params":["0x2068f8e9C9e61A330F2F713C998D372C04e3C9Cc","0xde0b6b3a7640000"],"id":1}' https://pre-rpc.myx.cash
+    const signers: SignerWithAddress[] = await hre.ethers.getSigners();
+    for (let signer of signers) {
+        // await hre.network.provider.send('hardhat_setBalance', [signer.address, '0xde0b6b3a7640000']);
+        // console.log(
+        //     `curl -X POST --data '{"jsonrpc":"2.0","method":"hardhat_setBalance","params":["${signer.address}","0xde0b6b3a7640000"],"id":1}' https://pre-rpc.myx.cash`,
+        // );
+        console.log(
+            signer.address + '_' + ethers.utils.formatEther(await deployer.provider.getBalance(signer.address)),
+        );
+    }
 }
 
 main().catch((error) => {
