58d4b9e231a5045e91ce14172edfd46645336abb
marvin <marvin@crus.xyz>
2023-11-02 10:25:36 +0800

    fix

--- scripts/deploy.dev.view.ts
+++ scripts/deploy.dev.view.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 import hre, { ethers } from 'hardhat';
-import { getOraclePriceFeed, getPool, getTokens } from '../helpers';
+import { getIndexPriceFeed, getOraclePriceFeed, getPool, getTokens } from '../helpers';
 import type { SignerWithAddress } from '@nomiclabs/hardhat-ethers/src/signers';
 
 async function main() {
@@ -13,31 +13,32 @@ async function main() {
     // const positionManager = await getPositionManager();
     // const executor = await getExecutor();
     // const executionLogic = await getExecutionLogic();
-    const priceOracle = await getOraclePriceFeed();
+    const oraclePriceFeed = await getOraclePriceFeed();
+    const indexPriceFeed = await getIndexPriceFeed();
     const pool = await getPool();
 
     const { btc, eth, usdt } = await getTokens();
 
-    const btcOraclePrice = ethers.utils.formatUnits(await priceOracle.getPrice(btc.address), 30);
-    const ethOraclePrice = ethers.utils.formatUnits(await priceOracle.getPrice(eth.address), 30);
-    const btcIndexPrice = ethers.utils.formatUnits(await priceOracle.getPrice(btc.address), 30);
-    const ethIndexPrice = ethers.utils.formatUnits(await priceOracle.getPrice(eth.address), 30);
+    const btcOraclePrice = ethers.utils.formatUnits(await oraclePriceFeed.getPrice(btc.address), 30);
+    const ethOraclePrice = ethers.utils.formatUnits(await oraclePriceFeed.getPrice(eth.address), 30);
+    const btcIndexPrice = ethers.utils.formatUnits(await indexPriceFeed.getPrice(btc.address), 30);
+    const ethIndexPrice = ethers.utils.formatUnits(await indexPriceFeed.getPrice(eth.address), 30);
     console.log(`btc price:`, btcOraclePrice);
     console.log(`eth price:`, ethOraclePrice);
     console.log(`btc price:`, btcIndexPrice);
     console.log(`eth price:`, ethIndexPrice);
 
     // curl -X POST --data '{"jsonrpc":"2.0","method":"hardhat_setBalance","params":["0x2068f8e9C9e61A330F2F713C998D372C04e3C9Cc","0xde0b6b3a7640000"],"id":1}' https://pre-rpc.myx.cash
-    const signers: SignerWithAddress[] = await hre.ethers.getSigners();
-    for (let signer of signers) {
-        // await hre.network.provider.send('hardhat_setBalance', [signer.address, '0xde0b6b3a7640000']);
-        // console.log(
-        //     `curl -X POST --data '{"jsonrpc":"2.0","method":"hardhat_setBalance","params":["${signer.address}","0xde0b6b3a7640000"],"id":1}' https://pre-rpc.myx.cash`,
-        // );
-        console.log(
-            signer.address + '_' + ethers.utils.formatEther(await deployer.provider.getBalance(signer.address)),
-        );
-    }
+    // const signers: SignerWithAddress[] = await hre.ethers.getSigners();
+    // for (let signer of signers) {
+    //     // await hre.network.provider.send('hardhat_setBalance', [signer.address, '0xde0b6b3a7640000']);
+    //     // console.log(
+    //     //     `curl -X POST --data '{"jsonrpc":"2.0","method":"hardhat_setBalance","params":["${signer.address}","0xde0b6b3a7640000"],"id":1}' https://pre-rpc.myx.cash`,
+    //     // );
+    //     console.log(
+    //         signer.address + '_' + ethers.utils.formatEther(await deployer.provider.getBalance(signer.address)),
+    //     );
+    // }
 }
 
 main().catch((error) => {
