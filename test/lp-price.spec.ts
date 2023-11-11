562e5b5c057a53f436a4d4552830de7c0092c8b1
marvin <marvin>
2023-11-11 17:15:38 +0800

    fix

--- test/lp-price.spec.ts
+++ test/lp-price.spec.ts
@@ -81,6 +81,40 @@ describe('LP: Price cases', () => {
         expect(new Decimal((await _lpPrice()).toString()).div(pricePrecision).toFixed(5)).to.be.eq('1.00000');
     });
 
+    it('test unbalanced, index > stable', async () => {
+        const {
+            oraclePriceFeed,
+            pool,
+            btc,
+            usdt,
+            router,
+            users: [depositor],
+        } = testEnv;
+        expect(new Decimal((await _lpPrice()).toString()).div(pricePrecision).toFixed(5)).to.be.eq('1.00000');
+
+        const expectLPAmount1 = ethers.utils.parseUnits('10000', 18);
+        await _addLiquidity(expectLPAmount1, depositor);
+        expect(new Decimal((await _lpPrice()).toString()).div(pricePrecision).toFixed(5)).to.be.eq('1.00000');
+
+        // only deposit index token
+        const oraclePrice = await oraclePriceFeed.getPrice(pair.indexToken);
+        const indexAmount = ethers.utils.parseUnits('1000', await btc.decimals());
+        await mintAndApprove(testEnv, btc, indexAmount, depositor, router.address);
+        await router
+            .connect(depositor.signer)
+            .addLiquidity(
+                pair.indexToken,
+                pair.stableToken,
+                indexAmount,
+                0,
+                [pair.indexToken],
+                [new ethers.utils.AbiCoder().encode(['uint256'], [oraclePrice.div('10000000000000000000000')])],
+                { value: 1 },
+            );
+
+        expect(new Decimal((await _lpPrice()).toString()).div(pricePrecision).toFixed(5)).to.be.eq('1.00000');
+    });
+
     async function _maxRemoveLiquidityAmount() {
         const { pool } = await _liquidityDelta();
 
@@ -98,6 +132,8 @@ describe('LP: Price cases', () => {
         const stableDeltaWad = await convertStableAmount(usdt, stableTotalAmount.sub(stableReservedAmount), 18);
 
         return {
+            index: indexDeltaWad,
+            stable: stableDeltaWad,
             pool: indexDeltaWad.add(stableDeltaWad),
             lp: (await lpToken.totalSupply()).mul(await _lpPrice()).div(pricePrecision),
         };
