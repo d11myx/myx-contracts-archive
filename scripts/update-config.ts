345e6bc4e3ad6b0ba6b37f71a15b1dacf6b3d6ce
marvin <marvin@crus.xyz>
2023-11-21 11:37:46 +0800

    feat: update pair config

---
+++ scripts/update-config.ts
@@ -0,0 +1,25 @@
+// @ts-ignore
+import { ethers } from 'hardhat';
+import { getPool, loadReserveConfig, MARKET_NAME } from '../helpers';
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    console.log(deployer.address);
+    console.log(await deployer.getBalance());
+
+    const pool = await getPool();
+
+    const reserveConfig = loadReserveConfig(MARKET_NAME);
+    const pairConfigs = reserveConfig?.PairsConfig;
+
+    const tradingConfig = pairConfigs['WETH'].tradingConfig;
+    // console.log(tradingConfig);
+
+    console.log(await pool.getTradingConfig(2));
+    // await pool.updateTradingConfig(2, tradingConfig);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
