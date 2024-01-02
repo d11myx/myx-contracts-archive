eef3029a24e96bebe3f021704809a106c4569809
marvin <marvin@crus.xyz>
2024-01-02 11:00:33 +0800

    fix: lp stable profit

--- scripts/update-config.ts
+++ scripts/update-config.ts
@@ -12,24 +12,11 @@ async function main() {
     const reserveConfig = loadReserveConfig(MARKET_NAME);
     const pairConfigs = reserveConfig?.PairsConfig;
 
-    const tradingConfig = pairConfigs['WBTC'].pair;
+    const tradingConfig = pairConfigs['WETH'].tradingConfig;
     // console.log(tradingConfig);
 
-    console.log(await pool.getPair(1));
-    // await pool.updatePair(1, {
-    //     pairIndex: 1,
-    //     indexToken: '0xA92368aE01D6203101F40b856d3341EdC6E78339',
-    //     stableToken: '0x93d67359A0f6F117150a70fDde6BB96782497248',
-    //     pairToken: '0x1EBb831953f57dB849753d27CC53492b39ee6D0f',
-    //     enable: true,
-    //     kOfSwap: ethers.utils.parseUnits('4.4', 46),
-    //     expectIndexTokenP: 50000000, //50%
-    //     maxUnbalancedP: 30000000, //10%
-    //     unbalancedDiscountRate: 100000, //0.1%
-    //     addLpFeeP: 100000, //0.1%
-    //     removeLpFeeP: 300000, //0.3%
-    //     lpFeeDistributeP: 0, //deprecated
-    // });
+    console.log(await pool.getPair(2));
+    await pool.updateTradingConfig(2, tradingConfig);
 }
 
 main().catch((error) => {
