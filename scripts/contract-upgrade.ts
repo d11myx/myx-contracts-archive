9ab3b17a8984d049347a83deda7401519aedc7d0
marvin <marvin@crus.xyz>
2024-01-18 15:56:45 +0800

    fix createOrder

--- scripts/contract-upgrade.ts
+++ scripts/contract-upgrade.ts
@@ -7,15 +7,18 @@ import {
     encodeParameters,
     getAddressesProvider,
     getFeeCollector,
+    getOrderManager,
     getPool,
     getPoolTokenFactory,
     getPositionManager,
     getRiskReserve,
     getTokens,
     latest,
+    ORDER_MANAGER_ID,
     PAIR_INFO_ID,
     POSITION_MANAGER_ID,
 } from '../helpers';
+import { deploy } from '@openzeppelin/hardhat-upgrades/dist/utils';
 
 async function main() {
     const { deployer } = await getNamedAccounts();
@@ -25,6 +28,7 @@ async function main() {
     const poolTokenFactory = await getPoolTokenFactory();
     const pool = await getPool();
     const positionManager = await getPositionManager();
+    const orderManager = await getOrderManager();
     const feeCollector = await getFeeCollector();
     const riskReserve = await getRiskReserve();
 
@@ -68,11 +72,27 @@ async function main() {
     //     ...COMMON_DEPLOY_PARAMS,
     // });
 
+    // await deployments.deploy(`${ORDER_MANAGER_ID}`, {
+    //     from: deployer,
+    //     contract: 'OrderManager',
+    //     args: [],
+    //     proxy: {
+    //         owner: deployer,
+    //         proxyContract: 'UUPS',
+    //         proxyArgs: [],
+    //         execute: {
+    //             methodName: 'initialize',
+    //             args: [addressesProvider.address, pool.address, positionManager.address],
+    //         },
+    //     },
+    //     ...COMMON_DEPLOY_PARAMS,
+    // });
+
     await hre.run('time-execution', {
-        target: positionManager.address,
+        target: orderManager.address,
         value: '0',
         signature: 'upgradeTo(address)',
-        data: encodeParameters(['address'], ['0xF26F796D855c2AE7395C04226B0fB12251694309']),
+        data: encodeParameters(['address'], ['0xFe1afE64349199fC432a3871944478A9e8c62767']),
         eta: Duration.seconds(13)
             .add(await latest())
             .toString(),
