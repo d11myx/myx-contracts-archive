f99943622786a0d38b808734ed1abc4b03ea9ad6
simple <simple>
2024-01-26 11:23:41 +0800

    fix

--- scripts/verify.ts
+++ scripts/verify.ts
@@ -36,27 +36,27 @@ async function main() {
     // });
     // console.log(artifact.address);
 
-    await verifyContract(local, '0xBDC545bD3C5615F16105fE2a357c77D3cB7bdCC6', []);
+    // await verifyContract(local, '0xBDC545bD3C5615F16105fE2a357c77D3cB7bdCC6', []);
 
-    // const arts = [
-    //     // 'Router',
-    //     // 'Executor',
-    //     // 'Pool_Implementation',
-    //     // 'PositionManager_Implementation',
-    //     // 'OrderManager_Implementation',
-    //     'ExecutionLogic',
-    //     // 'LiquidationLogic',
-    // ];
-    // for (let art of arts) {
-    //     try {
-    //         const deployment = await deployments.get(art);
-    //         // console.log(deployment.address);
-    //         // console.log(deployment.args);
-    //         await verifyContract(local, deployment.address, deployment.args);
-    //     } catch (e) {
-    //         console.log(e);
-    //     }
-    // }
+    const arts = [
+        // 'Router',
+        // 'Executor',
+        // 'Pool_Implementation',
+        // 'PositionManager_Implementation',
+        // 'OrderManager_Implementation',
+        'ExecutionLogic',
+        // 'LiquidationLogic',
+    ];
+    for (let art of arts) {
+        try {
+            const deployment = await deployments.get(art);
+            // console.log(deployment.address);
+            // console.log(deployment.args);
+            await verifyContract(local, deployment.address, deployment.args);
+        } catch (e) {
+            console.log(e);
+        }
+    }
 
     // await verifyProxyContract(lineaGoerli, '0xd304065B7F596034270356644FF0A220574979eD', [
     //     '0x063967b144abf07dAb4751d2556E2E8A70B78e80',
